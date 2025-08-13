6abc7e3c53c6768753da2720702903752f1849e9
YuriyNik156 <yuriy.nik156@gmail.com>
2025-08-13 15:39:05 +0300

    add main2.py and test2.py

---
+++ lesson/main.py
@@ -0,0 +1,8 @@
+import pytest
+
+def test_add2():
+    assert 1 + 1 == 2
+
+def test_sub2():
+    assert 3 - 2 == 1
+
--- lesson/test.py
+++ lesson/test.py
@@ -9,4 +9,3 @@ class TestMath(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
