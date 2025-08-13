7740469987cb57453a4ee2a7c3964dd6f028f0b2
YuriyNik156 <yuriy.nik156@gmail.com>
2025-08-13 18:31:55 +0300

    create fixture

--- lesson/fixtures/main5.py
+++ lesson/fixtures/main5.py
@@ -1,11 +1,11 @@
 import sqlite3
 
 def init_db():
-    conn = sqlite3.connect(':memory')
+    conn = sqlite3.connect(':memory:')
     cursor = conn.cursor()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
-    id INTEGER_PRIMARY_KEY, 
+    id INTEGER PRIMARY KEY, 
     name TEXT, 
     age INTEGER)
     ''')
