52343b48f4fde630262389933ff8c67ec2404760
YuriyNik156 <yuriy.nik156@gmail.com>
2025-08-14 11:58:22 +0300

    add code in main_hw.py

---
+++ hometask/main_hw.py
@@ -0,0 +1,10 @@
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for i, char in enumerate(s): # i - порядковый номер, char - буквенный символ
+        if char in vowels:
+            count += 1
+        elif char.lower() == 'y' and i != 0: # Проверяем, является ли буква "y" первой в слове
+            if s[i - 1] not in vowels and s[i - 1].lower() != 'y': # Проверяем, является ли буква перед буквой "y" гласной
+                count += 1
+    return count
